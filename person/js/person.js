aeefbd48bc0077d8c1cfc986009ea8097fac5e78
GalenoJiao <PUBG8012@163.com>
2021-06-16 10:52:38 +0800

    2023.06.16-1

---
+++ person/js/person.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-15 21:27:51
+ * @LastEditTime: 2021-06-15 21:27:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \class202310\js\person.js
+ */
